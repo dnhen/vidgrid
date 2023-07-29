bbb787c33d56713551dbfc2271662f5cac53784b
dnhen <69449713+dnhen@users.noreply.github.com>
2023-07-29 15:07:56 +1000

    Fixed pages

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import '@/styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>VidGrid</title>
+      </Head>
+    </>
+  );
+};
+
+export default Home;
