e504fdbb5a1aac0a1514f274ba6643df0aef1d39
dnhen <69449713+dnhen@users.noreply.github.com>
2023-08-02 11:28:30 +1000

    Added posthog

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/:path*',
+        destination: 'https://app.posthog.com/:path*',
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,20 @@ import { ControlsContextProvider } from '@/contexts/useControls';
 import '@/styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import posthog from 'posthog-js';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (location.hostname !== 'localhost' && location.hostname !== '127.0.0.1') {
+      // Get current URL for rewritre
+      const currentUrl = window.location.protocol + '//' + window.location.host;
+
+      // Load posthog
+      posthog.init('phc_9W4CGDF0Mv5LvxzD8akGij4uGG9ECBXSoUkfYG1j7DX', { api_host: `${currentUrl}/ingest` });
+    }
+  });
+
   return (
     <ControlsContextProvider>
       <ChannelsContextProvider>
