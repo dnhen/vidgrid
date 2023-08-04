e4e078f84a2e266dc919f05e9b3052599a198c68
dnhen <69449713+dnhen@users.noreply.github.com>
2023-08-04 22:46:46 +1000

    Added GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { ControlsContextProvider } from '@/contexts/useControls';
 import '@/styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import posthog from 'posthog-js';
 import { useEffect } from 'react';
 
@@ -21,6 +22,20 @@ export default function App({ Component, pageProps }: AppProps) {
     <ControlsContextProvider>
       <ChannelsContextProvider>
         <ChakraProvider>
+          <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=G-RYL4H4QV41`} />
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-RYL4H4QV41');
+              `,
+            }}
+          />
           <Component {...pageProps} />
         </ChakraProvider>
       </ChannelsContextProvider>
